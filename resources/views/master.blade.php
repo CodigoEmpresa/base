04c1b4b366ba12bd43fa5dd33f52ad13d23959f1
Jonatan <jonatancastro1@gmail.com>
2016-04-21 14:31:17 -0500

    Commit inicial

---
+++ resources/views/master.blade.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+
+
+
+<html lang="es">
+  <head>
+      <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="description" content="">
+      <meta name="author" content="">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+
+      <link rel="icon" href="Img/idrd_icon.ico">    
+      <link rel="stylesheet" href="public/Css/bootstrap.min.css" media="screen">    
+      <link rel="stylesheet" href="public/Css/sticky-footer.css" media="screen">    
+      <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+      <script src="public/Js/jquery.js"></script>
+      <script src="public/Js/bootstrap.min.js"></script>
+
+      <title>Nombre Módulo</title>
+  </head>
+
+  <body>
+      
+       <!-- Menu Módulo -->
+       <div class="navbar navbar-default navbar-fixed-top">
+        <div class="container">
+          <div class="navbar-header">
+            <a href="#" class="navbar-brand">SIM</a>
+            <button class="navbar-toggle" type="button" data-toggle="collapse" data-target="#navbar-main">
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+          </div>
+          <div class="navbar-collapse collapse" id="navbar-main">
+            <ul class="nav navbar-nav">
+              <li class="dropdown">
+                <a class="dropdown-toggle" data-toggle="dropdown" href="#" id="themes">Opción 1 <span class="caret"></span></a>
+                <ul class="dropdown-menu" aria-labelledby="themes">
+                  <li><a href="#">Default</a></li>
+                  <li class="divider"></li>
+                  <li><a href="#">Sub-Item 1</a></li>
+                  <li><a href="#">Sub-Item 2</a></li>
+                  <li><a href="#">Sub-Item 3</a></li>
+                  <li><a href="#">Sub-Item 4</a></li>
+                </ul>
+              </li>
+              <li>
+                <a href="#">Opción 2</a>
+              </li>
+              <li class="dropdown">
+                <a class="dropdown-toggle" data-toggle="dropdown" href="#" id="download">Opción 3 <span class="caret"></span></a>
+                <ul class="dropdown-menu" aria-labelledby="download">
+                  <li><a href="#">Default</a></li>
+                  <li class="divider"></li>
+                  <li><a href="#">Sub-Item 1</a></li>
+                  <li><a href="#">Sub-Item 2</a></li>
+                  <li><a href="#">Sub-Item 3</a></li>
+                  <li><a href="#">Sub-Item 4</a></li>
+                </ul>
+              </li>
+            </ul>
+
+            <form class="navbar-form navbar-left" role="search">
+                <div class="form-group">
+                  <input type="text" class="form-control" placeholder="Buscar">
+                </div>                
+                <button type="submit" class="btn btn-default">Ir</button>
+            </form>
+
+            <ul class="nav navbar-nav navbar-right">
+              <li><a href="http://www.idrd.gov.co/sitio/idrd/" target="_blank">I.D.R.D</a></li>
+              <li><a href="#" target="_blank">Cerrar Sesión</a></li>
+            </ul>
+
+          </div>
+        </div>
+      </div>
+      <!-- FIN Menu Módulo -->
+        
+      <!-- Contenedor información módulo -->
+      </br></br>
+      <div class="container">
+          <div class="page-header" id="banner">
+            <div class="row">
+              <div class="col-lg-8 col-md-7 col-sm-6">
+                <h1>MÓDULO</h1>
+                <p class="lead"><h1>##### ### ### ####</h1></p>
+              </div>
+              <div class="col-lg-4 col-md-5 col-sm-6">
+                 <div align="right"> 
+                    <img src="public/Img/IDRD.JPG" width="50%" heigth="40%"/>
+                 </div>                    
+              </div>
+            </div>
+          </div>        
+      </div>
+      <!-- FIN Contenedor información módulo -->
+
+      <!-- Contenedor panel principal -->
+      <div class="container">
+          <div class="panel panel-default">
+                <div class="panel-body">
+                   @yield('content')   
+                </div>
+          </div>
+      </div>        
+      <!-- FIN Contenedor panel principal -->
+
+      <footer class="footer">          
+          <div class="container">
+                         
+          </div>
+      </footer>          
+   
+      <script src="Js/jquery-1.10.2.min.js"></script>
+      <script src="Js/bootstrap.min.js"></script>
+  </body>
+
+</html>
+
+
+
+
+
