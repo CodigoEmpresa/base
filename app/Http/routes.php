087201e4d54ee51ab0a500621c25ef6de89d815e
Jonathan Castro <jonatancastro1@gmail.com>
2016-09-16 09:15:11 -0500

    Implementación de permisos para entorno local

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,5 +1,5 @@
 <?php
-
+session_start();
 /*
 |--------------------------------------------------------------------------
 | Routes File
@@ -10,34 +10,17 @@
 | and give it the controller to call when that URI is requested.
 |
 */
-
-Route::get('/', function () {                
-    return view('welcome');
-});
-
-Route::get('uno', function () {                
-    return 'welcome';
-});
-
-
 Route::get('/personas', '\Idrd\Usuarios\Controllers\PersonaController@index');
-
 Route::get('/personas/service/obtener/{id}', '\Idrd\Usuarios\Controllers\PersonaController@obtener');
 Route::get('/personas/service/buscar/{key}', '\Idrd\Usuarios\Controllers\PersonaController@buscar');
 Route::get('/personas/service/ciudad/{id_pais}', '\Idrd\Usuarios\Controllers\LocalizacionController@buscarCiudades');
 Route::post('/personas/service/procesar/', '\Idrd\Usuarios\Controllers\PersonaController@procesar');
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| This route group applies the "web" middleware group to every route
-| it contains. The "web" middleware group is defined in your HTTP
-| kernel and includes session state, CSRF protection, and more.
-|
-*/
+Route::any('/', 'MainController@index');
+Route::any('/logout', 'MainController@logout');
 
+//rutas con filtro de autenticación
 Route::group(['middleware' => ['web']], function () {
-    //
+	Route::get('/welcome', 'MainController@welcome');
 });
+/*
